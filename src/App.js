e7e82baf7072e5a25a70209d4a8d365c300c7b38
Laura Berge <lauraeberge@gmail.com>
2022-02-15 18:07:33 -0600

    post-lecture part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
-function App() {
+function App(props) {
+  const items = useSelector(state => state)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My cart</h1>
+      <ul>
+        {items.map(i => <li>{i}</li>)}
+      </ul>
     </div>
   );
 }
+
+// function mapStateToProps(state){
+// // return object that will be be merged with my props
+//   return {
+//     items: state
+//   }
+// }
 
 export default App;
---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+const initialState = [
+  "apples",
+  "pears",
+  "spinach",
+  "bread",
+  "peanut butter"
+]
+
+function reducer(state = initialState, action){
+  // switch determines what my new state is
+  return state;
+}
+
+export default reducer;
